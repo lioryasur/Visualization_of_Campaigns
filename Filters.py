5520bc1dcc2d92fbd1a389656ccfaa60d8b4a6da
FreddieMG <fredigr1998@gmail.com>
2023-06-24 01:15:21 +0300

    changed A

--- Filters.py
+++ Filters.py
@@ -2,81 +2,6 @@
 import pandas as pd
 import streamlit as st
 
-# def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-#     """
-#     Adds a UI on top of a dataframe to let viewers filter columns
-#
-#     Args:
-#         df (pd.DataFrame): Original dataframe
-#
-#     Returns:
-#         pd.DataFrame: Filtered dataframe
-#     """
-#     modify = st.checkbox("Add filters")
-#
-#     if not modify:
-#         return df
-#
-#     df = df.copy()
-#
-#     # Try to convert datetimes into a standard format (datetime, no timezone)
-#     for col in df.columns:
-#         if is_object_dtype(df[col]):
-#             try:
-#                 df[col] = pd.to_datetime(df[col])
-#             except Exception:
-#                 pass
-#
-#         if is_datetime64_any_dtype(df[col]):
-#             df[col] = df[col].dt.tz_localize(None)
-#
-#     modification_container = st.container()
-#
-#     with modification_container:
-#         to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
-#         for column in to_filter_columns:
-#             left, right = st.columns((1, 20))
-#             # Treat columns with < 10 unique values as categorical
-#             if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-#                 user_cat_input = right.multiselect(
-#                     f"Values for {column}",
-#                     df[column].unique(),
-#                     default=list(df[column].unique()),
-#                 )
-#                 df = df[df[column].isin(user_cat_input)]
-#             elif is_numeric_dtype(df[column]):
-#                 _min = float(df[column].min())
-#                 _max = float(df[column].max())
-#                 step = (_max - _min) / 100
-#                 user_num_input = right.slider(
-#                     f"Values for {column}",
-#                     min_value=_min,
-#                     max_value=_max,
-#                     value=(_min, _max),
-#                     step=step,
-#                 )
-#                 df = df[df[column].between(*user_num_input)]
-#             elif is_datetime64_any_dtype(df[column]):
-#                 user_date_input = right.date_input(
-#                     f"Values for {column}",
-#                     value=(
-#                         df[column].min(),
-#                         df[column].max(),
-#                     ),
-#                 )
-#                 if len(user_date_input) == 2:
-#                     user_date_input = tuple(map(pd.to_datetime, user_date_input))
-#                     start_date, end_date = user_date_input
-#                     df = df.loc[df[column].between(start_date, end_date)]
-#             else:
-#                 user_text_input = right.text_input(
-#                     f"Substring or regex in {column}",
-#                 )
-#                 if user_text_input:
-#                     df = df[df[column].astype(str).str.contains(user_text_input)]
-#
-#     return df
-
 def filter_A(df: pd.DataFrame) -> pd.DataFrame:
     """
     Adds a UI on top of a dataframe to let viewers filter columns
@@ -87,11 +12,34 @@ def filter_A(df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Filtered dataframe
     """
+
     modify = st.checkbox("Add filters")
 
     if not modify:
-        return df
-    df = df[df['population_bad'] == False]
+        left, right = st.columns((1, 20))
+
+        column = 'year'
+        _min = float(df[column].min())
+        _max = float(df[column].max())
+        step = (_max - _min) / 100
+        user_num_input = right.slider(
+            f"Values for {column}",
+            min_value=float(_min),
+            max_value=float(_max),
+            value=(float(1983), float(1993)),  # Set the default range
+            step=float(step),
+        )
+        df = df[df[column].between(*user_num_input)]
+        df.sort_values(by=['year'], inplace=True)
+        cy0id = df[df["cyear"] == 0]["id"]
+        cy1id = df[df["cyear"] == 1]["id"]
+
+        ids = set(cy0id).intersection(set(cy1id))
+        df = df[df["id"].isin(ids)]
+
+        return df, df["id"].unique()
+
+    df = df[df['percent_participation'] < 12]
     df = df.copy()
 
     modification_container = st.container()
@@ -117,13 +65,20 @@ def filter_A(df: pd.DataFrame) -> pd.DataFrame:
                 step = (_max - _min) / 100
                 user_num_input = right.slider(
                     f"Values for {column}",
-                    min_value=_min,
-                    max_value=_max,
-                    value=(_min, _max),
-                    step=step,
+                    min_value=float(_min),
+                    max_value=float(_max),
+                    value=(float(1983), float(1993)),  # Set the default range
+                    step=float(step),
                 )
                 df = df[df[column].between(*user_num_input)]
-    return df
+                df.sort_values(by=['year'], inplace=True)
+                cy0id = df[df["cyear"] == 0]["id"]
+                cy1id = df[df["cyear"] == 1]["id"]
+
+                ids = set(cy0id).intersection(set(cy1id))
+                df = df[df["id"].isin(ids)]
+
+    return df, df["id"].unique()
 
 
 def calculate_stat(df, stat_choice):
@@ -151,16 +106,30 @@ def filter_B(df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Filtered dataframe
     """
-    modify = st.checkbox("Choose statistic")
+    modify = st.checkbox("Choose statistic for calculating percent participation of campaign across its years")
+
+
+
     df = df.copy()
 
+    campaign_type = st.selectbox("Large or Small Campaigns (by percent of populations)", ['Large', 'Small'], index=1)
+
+
+
     df = df[df['population_bad'] == False]
+
     if not modify:
         df_new = df.groupby('id').apply(calculate_stat, stat_choice='Average').reset_index()  # Replace 'avg' with your actual choice
-        print(df_new)
         df_new.columns = ['id', 'stat', 'success']
 
+        df_new = df_new[df_new['stat'] < 15]
+        if campaign_type == 'Large':
+            df_new = df_new[df_new['stat'] > 2]
+        else:
+            df_new = df_new[df_new['stat'] < 2]
+
         return df_new
+
 
 
     modification_container = st.container()
@@ -171,7 +140,14 @@ def filter_B(df: pd.DataFrame) -> pd.DataFrame:
         print(df)
         df.columns = ['id', 'stat', 'success']
 
+    df = df[df['stat'] < 15]
+    if campaign_type == 'Large':
+        df = df[df['stat'] > 2]
+    else:
+        df = df[df['stat'] < 2]
+
     return df
+
 # Define the user's choice for the statistic
 stat_choice = 'avg'  # Change this to 'avg', 'max', 'sum', or 'last' based on the user's choice
 
